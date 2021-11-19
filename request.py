3a4f69a89f28a73aee87690d274c47a3b37f8da0
Krawcode <54539896+Krawcode@users.noreply.github.com>
2021-11-19 14:21:57 +0700

    Add files via upload

---
+++ request.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3  
+
+#==================================== Library need install =====================================#
+
+#pip3 install pymongo 
+#pip3 install tqdm
+#pip3 install pandas 
+
+#==================================== End library need install =================================#
