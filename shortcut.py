df4173a4dbcce54a1f7287fee1a7da95a1bcc2e5
Krawcode <54539896+Krawcode@users.noreply.github.com>
2021-11-19 14:04:50 +0700

    Add files via upload

---
+++ shortcut.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3  
+
+
+#==================================== Shortcut Setting =====================================#
+
+#------------------------------------ System key -------------------------------------------#
+
+exit_program = {'7','e','exit'} 
+help_menu = {'8','h','help'} 
+how_to_use_this_program = {'9','ex','example'} 
+home_menu = {'0','m','home','menu'} 
+back_menu = {'b','back'} 
+
+#------------------------------------ 0st Tree Degree ---------------------------------------#
+
+check = {'1','c','check'}
+update = {'2','up','update'}
+search = {'3','f','search','find'}
+download = {'4','s','save','export','download'}
+delete = {'5','d','del','delete'}
+
+#------------------------------------ 1st Tree Degree ---------------------------------------#
+
+# updateDB
+donot_replace = {'1','nore','noreplace'}
+replace = {'2','re','replace'}
+new_data = {'3','new','newdoc','newdata'}
+# load by csv index or find by condition
+search_condition = {'1','f','c','find','condition'}
+download_data_by_csv = {'2','l','csv','load'}
+# findDB --> condition
+# deleteDB & saveDB 2st & findDB 2st
+save_or_delete = {'1','d'}
+save_or_delete_file = {'2','df'}
+
+#------------------------------------ 2st Tree Degree ---------------------------------------#
+
+# updateDB --> path
+# findDB
+delete_data = {'4','deld'}
+delete_data_and_file = {'5','deldf'}
+
+#==================================== End Setting ===========================================#
