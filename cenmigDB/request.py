60e9c0c5e35ea4e0d842c428c04a9cc537da7629
Krawcode <54539896+Krawcode@users.noreply.github.com>
2021-11-19 13:49:17 +0700

    Delete cenmigDB directory

--- cenmigDB/request.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3  
-
-#==================================== Library need install =====================================#
-
-#pip3 install pymongo 
-#pip3 install tqdm
-#pip3 install pandas 
-
-#==================================== End library need install =================================#
