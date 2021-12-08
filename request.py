2b7efec79afc8ea4239c2cfade749d0c307aa965
Krawcode <54539896+Krawcode@users.noreply.github.com>
2021-12-08 14:12:04 +0700

    Update request.py

--- request.py
+++ request.py
@@ -5,5 +5,7 @@
 #pip3 install pymongo 
 #pip3 install tqdm
 #pip3 install pandas 
+#pip3 install glob2
+#pip3 install argparse
 
 #==================================== End library need install =================================#
