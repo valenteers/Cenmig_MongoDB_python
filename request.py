21ea8bd836476db43092e2935f6374b5a63948bd
liria <36659752+valenteers@users.noreply.github.com>
2021-12-09 12:31:38 +0700

    Add files via upload

---
+++ request.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3  
+
+#==================================== Library need install =====================================#
+
+#pip3 install pymongo 
+#pip3 install tqdm
+#pip3 install pandas 
+#pip3 install glob2
+#pip3 install argparse
+
+#==================================== End library need install =================================#
